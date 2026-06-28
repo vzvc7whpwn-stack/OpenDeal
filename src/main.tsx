4643f56504ed85349dbf2b48612683278696f148
Mitch <vzvc7whpwn@privaterelay.appleid.com>
2026-06-28 03:16:19 -0400

    fix: import tailwind css to restore app design

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import './index.css';  // <-- THIS IS THE MAGIC LINE!
 import App from './App';
 
 createRoot(document.getElementById('root')!).render(
